a9933cc85a73e98e1857135eb30fbbdd63f98dbb
krvspkumar <RVSPKumar.Kakumani@ltimindtree.com>
2024-10-18 18:46:32 +0530

    ok

--- src/main/java/org/ltimindtree/configuration/AppConfig.java
+++ src/main/java/org/ltimindtree/configuration/AppConfig.java
@@ -1,65 +1,47 @@
-//package org.ltimindtree.configuration;
-//
-//import com.amazonaws.auth.InstanceProfileCredentialsProvider;
-//import com.amazonaws.client.builder.AwsClientBuilder;
-//import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import okhttp3.OkHttpClient;
-//
-//import java.net.URI;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-//import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-//import software.amazon.awssdk.regions.Region;
-//import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-//
-//@Configuration
-//public class AppConfig {
-//
-//	@Value("${aws-access-key-id}")
-//	private String awsAccessKey;
-//
-//	@Value("${aws-secret-access-key}")
-//	private String awsSecretKey;
-//
-//	@Value("${aws-endpoint}")
-//	private String awsDynamoDBEndPoint;
-//
-//	@Value("${aws-region}")
-//	private String awsRegion;
-//
-//	@Bean
-//	public OkHttpClient okHttpClient() {
-//		return new OkHttpClient();
-//	}
-//
-//	@Bean
-//	public ObjectMapper objectMapper() {
-//		return new ObjectMapper();
-//	}
-//
-////	@Bean
-////	public DynamoDbClient dynamoDbClient() {
-////		return DynamoDbClient.create();
-////	}
-//
-//	@Bean
-//	public DynamoDbClient dynamoDbClient() {
-//		AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard()
-//				.withCredentials(new InstanceProfileCredentialsProvider(false));
-//
-//		if (!awsDynamoDBEndPoint.isEmpty()) {
-//			builder.withEndpointConfiguration(
-//					new AwsClientBuilder.EndpointConfiguration(awsDynamoDBEndPoint, "us-east-1"));
-//		}
-//
-//		return DynamoDbClient.builder().endpointOverride(URI.create(awsDynamoDBEndPoint)).region(Region.US_EAST_1)
-//				.credentialsProvider(
-//						StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKey, awsSecretKey)))
-//				.build();
-//	}
-//}
+package org.ltimindtree.configuration;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.OkHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class AppConfig {
+
+	@Value("${aws-endpoint}")
+	private String awsDynamoDBEndPoint;
+
+	@Bean
+	public OkHttpClient okHttpClient() {
+		return new OkHttpClient();
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
+
+	@Bean
+	public DynamoDbClient dynamoDbClient() {
+		AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard()
+				.withCredentials(new InstanceProfileCredentialsProvider(false));
+
+		if (!awsDynamoDBEndPoint.isEmpty()) {
+			builder.withEndpointConfiguration(
+					new AwsClientBuilder.EndpointConfiguration(awsDynamoDBEndPoint, "us-east-1"));
+		}
+
+		return DynamoDbClient.builder().endpointOverride(URI.create(awsDynamoDBEndPoint)).region(Region.US_EAST_1)
+				.build();
+	}
+}
--- src/main/java/org/ltimindtree/configuration/DynamoDbConfig.java
+++ src/main/java/org/ltimindtree/configuration/DynamoDbConfig.java
@@ -1,27 +1,45 @@
-package org.ltimindtree.configuration;
-
-import java.net.URI;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import com.amazonaws.auth.InstanceProfileCredentialsProvider;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import okhttp3.OkHttpClient;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-
-@Configuration
-public class DynamoDBConfig {
-
-	@Value("${aws-endpoint}")
-	private String awsDynamoDBEndPoint;
-
+//package org.ltimindtree.configuration;
+//
+//import java.net.URI;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+//import com.amazonaws.client.builder.AwsClientBuilder;
+//import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//import okhttp3.OkHttpClient;
+//import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+//
+//@Configuration
+//public class DynamoDBConfig {
+//
+//	@Value("${aws-endpoint}")
+//	private String awsDynamoDBEndPoint;
+//
+////	@Bean
+////	public AmazonDynamoDB amazonDynamoDB() {
+////		AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard()
+////				.withCredentials(new InstanceProfileCredentialsProvider(false));
+////
+////		if (!awsDynamoDBEndPoint.isEmpty()) {
+////			builder.withEndpointConfiguration(
+////					new AwsClientBuilder.EndpointConfiguration(awsDynamoDBEndPoint, "us-east-1"));
+////		}
+////
+////		return builder.build();
+////	}
+////
+////	@Bean
+////	public DynamoDBMapper mapper() {
+////		return new DynamoDBMapper(amazonDynamoDB());
+////	}
+//
 //	@Bean
-//	public AmazonDynamoDB amazonDynamoDB() {
+//	public DynamoDbClient dynamoDbClient() {
 //		AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard()
 //				.withCredentials(new InstanceProfileCredentialsProvider(false));
 //
@@ -30,34 +48,16 @@ public class DynamoDBConfig {
 //					new AwsClientBuilder.EndpointConfiguration(awsDynamoDBEndPoint, "us-east-1"));
 //		}
 //
-//		return builder.build();
+//		return DynamoDbClient.builder().endpointOverride(URI.create(awsDynamoDBEndPoint)).build();
 //	}
 //
 //	@Bean
-//	public DynamoDBMapper mapper() {
-//		return new DynamoDBMapper(amazonDynamoDB());
+//	public OkHttpClient okHttpClient() {
+//		return new OkHttpClient();
 //	}
-
-	@Bean
-	public DynamoDbClient dynamoDbClient() {
-		AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard()
-				.withCredentials(new InstanceProfileCredentialsProvider(false));
-
-		if (!awsDynamoDBEndPoint.isEmpty()) {
-			builder.withEndpointConfiguration(
-					new AwsClientBuilder.EndpointConfiguration(awsDynamoDBEndPoint, "us-east-1"));
-		}
-
-		return DynamoDbClient.builder().endpointOverride(URI.create(awsDynamoDBEndPoint)).build();
-	}
-
-	@Bean
-	public OkHttpClient okHttpClient() {
-		return new OkHttpClient();
-	}
-
-	@Bean
-	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
-	}
-}
+//
+//	@Bean
+//	public ObjectMapper objectMapper() {
+//		return new ObjectMapper();
+//	}
+//}
