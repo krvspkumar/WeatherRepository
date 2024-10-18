0fdee6a351d11538f17aafc476b8c45a745893c9
krvspkumar <RVSPKumar.Kakumani@ltimindtree.com>
2024-10-18 18:08:47 +0530

    ok ok

--- src/main/java/org/ltimindtree/service/WeatherService.java
+++ src/main/java/org/ltimindtree/service/WeatherService.java
@@ -58,7 +58,7 @@ public class WeatherService implements WeatherRepository {
 
 		// If not in cache or expired, call the API
 		WeatherResponse weatherResponse = callWeatherApi(cityName);
-		weatherResponse.setCityName(cityName);
+		//weatherResponse.setCityName(cityName);
 		//weatherResponse.setTimestamp(Instant.now().getEpochSecond());
 
 		// Cache the result in DynamoDB
